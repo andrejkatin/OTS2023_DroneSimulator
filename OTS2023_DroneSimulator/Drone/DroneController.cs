2158be2aba14dfc82152ce4f7f9f4f89bda875b5
L-26\Andrej <katin@uns.ac.rs>
2023-03-21 15:05:53 +0100

    Add project files.

---
+++ OTS2023_DroneSimulator/Drone/DroneController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2023_DroneSimulator
+{
+    public class DroneController
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
